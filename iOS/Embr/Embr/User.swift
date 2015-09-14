b88e8f8fb497796f02cc8b9d2dc8fd5aaaab740d
alexronquillo <alex.ronquillo21@yahoo.com>
2015-09-14 18:43:45 -0400

    Initial commit

---
+++ iOS/Embr/Embr/User.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+class User {
+    var id: String = "\(rand())"
+    var username: String? = nil
+    var friends: [User] = []
+    var parentalControls: ParentalControls?
+    
+    init() {
+        id = "\(rand())"
+    }
+    
+    init(id: String, username: String, friends: [User], parentalControls: ParentalControls) {
+        self.id = id
+        self.username = username
+        self.friends = friends
+        self.parentalControls = parentalControls
+    }
+    
+    func addFriend(friend: User) {
+        var alreadyAFriend = false
+        for user in friends {
+            if user.equals(friend) {
+                alreadyAFriend = true
+            }
+        }
+        if !alreadyAFriend {
+            friends.append(friend)
+        }
+    }
+    
+    func removeFriend(friendUsername: String) {
+        var removeIndex: Int? = nil
+        let max = friends.count - 1
+        for (var i = 0; i <= max; ++i) {
+            removeIndex = i
+            break
+        }
+        if removeIndex != nil {
+            friends.removeAtIndex(removeIndex!)
+        }
+    }
+    
+    func equals(anotherUser: User) -> Bool {
+        return id == anotherUser.id
+    }
+}
+
+class ParentalControls {
+    
+}
