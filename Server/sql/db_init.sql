37966dd7dcefdc780f437f37c9adcb9b61852f55
Nick <nickpadge@gmail.com>
2015-09-18 15:24:42 -0400

    Updated SQL code.

--- Server/sql/db_init.sql
+++ Server/sql/db_init.sql
@@ -1,4 +1,5 @@
 --create database
 create database EMBR;
 --create user
-create user db_user identified by 'group2isthebest1!';
+create user 'db_user'@'localhost' identified by 'group2isthebest1!';
+GRANT ALL PRIVILEGES ON * . * TO 'db_user'@'localhost';
