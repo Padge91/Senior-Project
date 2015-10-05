b9182948abe27b43d6981d072e6c447d29299063
Nick <nickpadge@gmail.com>
2015-10-05 16:55:05 -0400

    Fixed numerous bugs. Added stability and clarity. Last step is to write objects to database. May need to create a handful of fake users to apply ratings to.

--- Server/python/API/apiIntegration.py
+++ Server/python/API/apiIntegration.py
@@ -17,11 +17,12 @@ password='group2isthebest1!'
 db='EMBR'
 
 #dont change these
-#alphabet = "a"
-alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+alphabet = "a"
+#alphabet = "abcdefghijklmnopqrstuvwxz1234567890"
 item_ids = []
 objects = []
-api_string = "http://api.walmartlabs.com/v1/search?apiKey={0}&query={1}&categoryId={2}"
+num_items = 10
+api_string = "http://api.walmartlabs.com/v1/search?apiKey={0}&query={1}&categoryId={2}&numItems="+str(num_items)
 object_api_string = "http://api.walmartlabs.com/v1/items?ids={0}&apiKey={1}"
 time_wait = .3
 
@@ -35,22 +36,50 @@ def main():
     print "Got object information"
 
     print "Inserting objects into database"
-    #insert_objects_into_db(objects)
+    insert_objects_into_db(objects)
     print "Done"
 
 def insert_object(cursor, object):
+    new_object = get_object_values(object)
+    print new_object
+    #query to insert item
+    #query to insert scores
     query1 = ""
     queries = []
     for query in queries:
         cursor.execute(query)
         cursor.commit()
 
+def get_object_values(object):
+    review_num_dict = {"name":"numReviews", "default":0, "fields":["numReviews"]}
+    review_score_dict = {"name":"reviewScore","default":0, "fields":["customerRating"]}
+    brand_dict = {"name":"brand", "default":None, "fields":["brandName"]}
+    description_dict = {"name":"description","default":None, "fields":["shortDescription","longDescription"]}
+    images_dict = {"name":"image","default":None, "fields":["thumbnailImage", "mediumImage", "largeImage"]}
+    name_dict = {"name":"name","default":None, "fields":["name"]}
+    category_dict = {"name":"category","default":None, "fields":["categoryPath"]}
+    #needs genres
+
+    dicts = [review_num_dict, review_score_dict, brand_dict, description_dict, images_dict, name_dict, category_dict]
+    object_dict = dict()
+    for dictionary in dicts:
+        for attempt_field in dictionary["fields"]:
+            if attempt_field in object:
+                object_dict[dictionary["name"]] = object[attempt_field]
+                break;
+            else:
+                object_dict[dictionary["name"]] = dictionary["default"]
+
+    return object_dict
+
+
+
 def insert_objects_into_db(objects):
     conn = pymysql.connect(host=host, port=port, user=user, passwd=password, db=db)
     cursor = conn.cursor()
 
     for object in objects:
-        insert_object(cursor, object)
+        insert_object(cursor, object["items"][0])
 
     cursor.close()
     conn.close()
@@ -81,12 +110,37 @@ def get_item_info(item_ids):
         response = make_request(url)
         time.sleep(time_wait);
         object = json.loads(response)
+        if not check_type(object["items"][0]):
+            continue
         objects.append(object)
 
+def check_type(object):
+    film_words = ["film", "Film", "Movie", "movie"]
+    tv_words = ["TV", "tv", "Tv", "television"]
+    book_words = ["book", "Book"]
+    object_category = object["categoryPath"]
+    return_val = False
+    if "OLD STUFF" in object_category:
+        return return_val
+    for word in film_words:
+        if word in object_category:
+            return_val = "Film"
+    for word in tv_words:
+        if word in object_category:
+            return_val = "TV"
+    for word in book_words:
+        if word in object_category:
+            return_val = "Book"
+
+    object["categoryPath"] = return_val
+
+    return return_val
+
+
 def iterate_through_pages(url, number_of_items):
-    iterations = int(number_of_items / 10)+1
+    iterations = int(number_of_items / num_items)+1
     for i in range(0, iterations):
-        new_url = url+"&start="+str((i*10)+1)
+        new_url = url+"&start="+str((i*num_items)+1)
         response_json = make_request(url)
         time.sleep(time_wait)
         print "Page Number " + str(i+1) + "/" + str(iterations)
