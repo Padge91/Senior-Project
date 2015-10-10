dcde9736cd3bf1b3637b3ab936685d546eef15a2
tdavi156 <tdavi156@students.kennesaw.edu>
2015-10-10 00:56:18 -0400

    add to libraries

--- Android/app/src/main/java/com/afe/pc/embr/ItemView.java
+++ Android/app/src/main/java/com/afe/pc/embr/ItemView.java
@@ -1,48 +1,60 @@
 package com.afe.pc.embr;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class ItemView extends AppCompatActivity {
 
     ListView listView;
+    Button addToLibraryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_view_layout);
 
-        // Get ListView object from xml
+        // List view
         listView = (ListView) findViewById(R.id.list1);
-
-        // Defined Array values to show in ListView
         String[] values = new String[] {"Comment 1", "Comment 2", "Comment 3", "Comment 4",
                 "Comment 5", "Comment 6", "Comment 7", "Comment 8", "Comment 9", "Comment 10",
                 "Comment 11", "Comment 12", "Comment 13", "Comment 14", "Comment 15", "Comment 16"};
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        // Assign adapter to ListView
         listView.setAdapter(adapter);
-
-        // ListView Item Click Listener
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                // ListView Clicked item value
                 String itemValue = (String) listView.getItemAtPosition(position);
                 openCommentActivity(itemValue);
+            }
+        });
+
+        // PopUp for the add to libraries button
+        addToLibraryButton = (Button) findViewById(R.id.addToLibraryButton);
+        addToLibraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(ItemView.this, addToLibraryButton);
+                popup.getMenuInflater().inflate(R.menu.menu_library, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        addToLibraryButtonStartActivity(item.toString());
+                        return true;
+                    }
+                });
+                popup.show();
             }
         });
     }
@@ -69,15 +81,36 @@ public class ItemView extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void addToLibrariesButton(View view) {
-        Toast.makeText(getApplicationContext(),
-                "button is working", Toast.LENGTH_LONG)
-                .show();
-        //Intent intent = new Intent(this, Library.class);
-        //startActivity(intent);
+    public void showLibraryPopUp(View view){
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        MenuInflater menuInflater = popupMenu.getMenuInflater();
+        PopUpMenuEventHandle popUpMenuEventHandle = new PopUpMenuEventHandle(getApplicationContext());
+        popupMenu.setOnMenuItemClickListener(popUpMenuEventHandle);
+        menuInflater.inflate(R.menu.menu_library, popupMenu.getMenu());
+        popupMenu.show();
     }
 
-    public void openCommentActivity(String S) {
+    public void addToLibraryButtonStartActivity(String s) {
+        Intent intent;
+        switch(s) {
+            case "Owend":
+                intent = new Intent(this, Library.class);
+                break;
+            case "Wishlist":
+                intent = new Intent(this, Library.class);
+                break;
+            case "Custom":
+                intent = new Intent(this, Library.class);
+                break;
+            default:
+                Toast.makeText(ItemView.this, "Not Available", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, HomeActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+
+    public void openCommentActivity(String s) {
         Intent intent = new Intent(this, CommentView.class);
         startActivity(intent);
     }
---
+++ Android/app/src/main/java/com/afe/pc/embr/PopUpLibraryButton.java
@@ -0,0 +1,58 @@
+package com.afe.pc.embr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import static android.support.v4.app.ActivityCompat.startActivity;
+
+public class PopUpLibraryButton implements PopupMenu.OnMenuItemClickListener {
+    Context context;
+
+    public PopUpLibraryButton(Context context) {
+        this.context = context;
+    }
+
+    /*public void openActivity(String S) {
+        if (S == "Home") {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        } else if (S == "RecommendedItems") {
+            Intent intent = new Intent(this, RecommendedItems.class);
+            startActivity(intent);
+        } else if (S == "Library") {
+            Intent intent = new Intent(this, Library.class);
+            startActivity(intent);
+        } else if (S == "SearchResults") {
+            Intent intent = new Intent(this, SearchResults.class);
+            startActivity(intent);
+        }
+    }*/
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Home:
+                Intent intent = new Intent(String.valueOf(HomeActivity.class));
+                context.startActivity(intent);
+                Toast.makeText(context, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Recommended_Items:
+                Intent intent1 = new Intent(String.valueOf(RecommendedItems.class));
+                context.startActivity(intent1);
+                Toast.makeText(context, "Recommended Items", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Libraries:
+                Intent intent2 = new Intent(String.valueOf(Library.class));
+                context.startActivity(intent2);
+                Toast.makeText(context, "Library", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
+
+
