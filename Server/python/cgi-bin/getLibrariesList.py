d390a7c1286dc3fe7800de95610a90b73b69e639
Nick <nickpadge@gmail.com>
2015-10-13 15:51:43 -0400

    Revert "Removed obsolete files."
    This reverts commit 413adb5daa05b7661ec6ea8b482cf7b0f9b04c87.

---
+++ Server/python/cgi-bin/getLibrariesList.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+__author__ = 'nicholaspadgett'
+
+import cgi
+from Utilities import *
+from userUtils import *
+from LibraryORM import *
+
+required_params = ["session", "user_id"]
+
+def main():
+    try:
+        form = get_required_parameters(cgi, required_params)
+        form["current_user_id"] = get_user_id_from_session(form)
+        orm = LibraryORM()
+        response = orm.get_user_libraries(form);
+        success_response(response)
+    except Exception as e:
+        failure_response(e.message)
+
+
+if __name__=="__main__":
+    main()
---
+++ Server/python/cgi-bin/submitReview.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+__author__ = 'nicholaspadgett'
+
+import pymysql
+import cgi
+import json
+from ItemORM import *
+from Utilities import *
+
+required_params = ["session","item_id", "score"]
+
+def main():
+    try:
+        form = get_required_parameters(cgi, required_params)
+        orm = ItemORM()
+        response = orm.add_score(form)
+        success_response(response)
+    except Exception as e:
+        failure_response(e.message)
+
+if __name__=="__main__":
+    main()
