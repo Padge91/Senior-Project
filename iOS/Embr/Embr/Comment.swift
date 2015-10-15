d09ba897ae1799afcaaa22c286c758f29a804011
alexronquillo <alex.ronquillo21@yahoo.com>
2015-10-15 15:32:52 -0400

    Removed subject from comments to match database

--- iOS/Embr/Embr/Comment.swift
+++ iOS/Embr/Embr/Comment.swift
@@ -3,17 +3,15 @@ import Foundation
 class Comment {
     private(set) var id: String
     private(set) var author: User
-    private(set) var subject: String
     private(set) var body: String
     private(set) var flagged: Bool
     private(set) var parent: Comment?
     private(set) var rating: Int
     private(set) var children: [Comment]
     
-    init(id: String, userWhoWroteTheComment author: User, subject: String, body: String, isFlaggedAsInappropriate flagged: Bool = false, parentComment parent: Comment?, rating: Int = 0, children: [Comment] = [Comment]()) {
+    init(id: String, userWhoWroteTheComment author: User, body: String, isFlaggedAsInappropriate flagged: Bool = false, parentComment parent: Comment?, rating: Int = 0, children: [Comment] = [Comment]()) {
         self.id = id
         self.author = author
-        self.subject = subject
         self.body = body
         self.flagged = flagged
         self.parent = parent
@@ -28,9 +26,9 @@ class Comment {
     func toString() -> String {
         if parent != nil {
             let parentComment = "\(parent!.author.username) said: \(parent!.body.substringToIndex(parent!.body.startIndex.advancedBy(20)))..."
-            return "\(parentComment)\n\n\(subject)\n\(body)\n-\(author.username)"
+            return "\(parentComment)\n\n\(body)\n-\(author.username)"
         } else {
-            return "\(subject)\n\(body)\n-\(author.username)"
+            return "\(body)\n-\(author.username)"
         }
     }
 }
