585526a4dc6e019c064d1e037b4ea74919951c08
alexronquillo <alex.ronquillo21@yahoo.com>
2015-10-17 09:49:44 -0400

    Added login prompt for access to libraries

--- iOS/Embr/Embr/ItemDetailsViewController.swift
+++ iOS/Embr/Embr/ItemDetailsViewController.swift
@@ -45,7 +45,7 @@ class ItemDetailsViewController: UIViewController, UITableViewDataSource, UITabl
     
     private func setupNavigation() {
         let menuItem = UIBarButtonItem(title: "Menu", style: .Plain, target: self, action: "goToMenu")
-        let librariesItem = UIBarButtonItem(title: "My Libraries", style: .Plain, target: self, action: "goToLibraries")
+        let librariesItem = UIBarButtonItem(title: "My Libraries", style: .Plain, target: self, action: "attemptToOpenLibraries")
         navigationItem.rightBarButtonItem = menuItem
         let librariesButton = librariesItem
         toolbarItems = [librariesButton]
@@ -149,6 +149,10 @@ class ItemDetailsViewController: UIViewController, UITableViewDataSource, UITabl
             if let destination = segue.destinationViewController as? CreateCommentViewController {
                 destination.mediaItem = self.mediaItem!
             }
+        } else if segue.identifier == librariesSegueIdentifier && sender is NSArray {
+            let destination = segue.destinationViewController as! LibrariesListTableViewController
+            let libraries = parseLibraryList(librariesArray: sender as! NSArray)
+            destination.librariesList = libraries
         }
     }
     
@@ -177,7 +181,16 @@ class ItemDetailsViewController: UIViewController, UITableViewDataSource, UITabl
         }
     }
     
-    @IBAction func addToLibrary(sender: UIButton) {
+    @IBAction func attemptAddToLibrary(sender: UIButton) {
+        if SessionModel.getSession() != SessionModel.noSession {
+            addToLibrary()
+        } else {
+            promptUserLogin()
+        }
+    }
+    
+    
+    func addToLibrary() {
         UserDataSource.getUserId { (data, response, error) -> Void in
             if data != nil {
                 do {
@@ -230,17 +243,101 @@ class ItemDetailsViewController: UIViewController, UITableViewDataSource, UITabl
         performSegueWithIdentifier(commentCreatorSegueIdentifier, sender: nil)
     }
     
-    func goToLibraries() {
+    func attemptToOpenLibraries() {
         if SessionModel.getSession() != SessionModel.noSession {
-            performSegueWithIdentifier(librariesSegueIdentifier, sender: nil)
+            goToLibraries()
         } else {
             promptUserLogin()
         }
     }
     
+    func goToLibraries() {
+        UserDataSource.getUserId { (data, response, error) -> Void in
+            if data != nil {
+                do {
+                    if let jsonResponse = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? NSDictionary {
+                        if jsonResponse["success"] as! Bool {
+                            let userId = jsonResponse["response"] as! Int
+                            LibrariesDataSource.getMyLibraries(userId, completionHandler: { (data, response, error) -> Void in
+                                if data != nil {
+                                    do {
+                                        let jsonResponse = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                                        if jsonResponse["success"] as! Bool {
+                                            if let response = jsonResponse["response"] as? NSArray {
+                                                if response.count > 0 {
+                                                    dispatch_async(dispatch_get_main_queue()) {
+                                                        self.performSegueWithIdentifier(self.librariesSegueIdentifier, sender: response)
+                                                    }
+                                                }
+                                            } else {
+                                                let jsonError = jsonResponse["response"]
+                                                let errorMsg = "Invalid response from GetLibrariesList.py:\n\(jsonError)"
+                                                log(logType: .Error, message: errorMsg)
+                                            }
+                                        } else {
+                                            let jsonError = jsonResponse["response"]
+                                            let errorMsg = "Invalid response from GetLibrariesList.py:\n\(jsonError)"
+                                            log(logType: .Error, message: errorMsg)
+                                        }
+                                    } catch {
+                                        let errorMsg = "Invalid data from GetLibrariesList.py"
+                                        log(logType: .Error, message: errorMsg)
+                                    }
+                                }
+                            })
+                        }
+                    }
+                } catch {
+                    let errorMsg = "Invalid data from GetUserIdFromSession.py"
+                    log(logType: .Error, message: errorMsg)
+                }
+            }
+        }
+    }
+    
+    private func alertError(errorMessage error: String) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        NSOperationQueue.mainQueue().addOperationWithBlock {
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func succesfulLogin(sessionId session: String) {
+        NSOperationQueue.mainQueue().addOperationWithBlock {
+            SessionModel.storeSession(sessionId: session)
+        }
+    }
+    
+    func loginCompletionHandler (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void {
+        if data != nil {
+            do {
+                let response = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! NSDictionary
+                if response["success"] as! Bool {
+                    if let session = response["response"] as? String {
+                        succesfulLogin(sessionId: session)
+                    }
+                } else if response["error"] != nil {
+                    alertError(errorMessage: "Invalid login")
+                }
+            } catch {
+                alertError(errorMessage: "Invalid response")
+            }
+        }
+    }
+    
     private func promptUserLogin() {
         let alert = UIAlertController(title: "Login", message: "Login required to access this content.", preferredStyle: .Alert)
-        let loginAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addTextFieldWithConfigurationHandler({(textfield: UITextField) in textfield.placeholder = "Username"})
+        alert.addTextFieldWithConfigurationHandler({(textfield: UITextField) in
+            textfield.secureTextEntry = true
+            textfield.placeholder = "Password"
+        })
+        let loginAction = UIAlertAction(title: "Login", style: .Default, handler: {(action: UIAlertAction) in
+            if let username = alert.textFields![0].text, password = alert.textFields![1].text {
+                UserDataSource.attemptLogin(username, password: password, completionHandler: self.loginCompletionHandler)
+            }
+        })
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         alert.addAction(loginAction)
         alert.addAction(cancelAction)
