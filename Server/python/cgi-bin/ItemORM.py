c9dc9614558ffcc5d13f40f81da5dbb35f98a374
Nick <nickpadge@gmail.com>
2015-10-17 11:08:04 -0400

    Reverted back to 10 items.

--- Server/python/cgi-bin/ItemORM.py
+++ Server/python/cgi-bin/ItemORM.py
@@ -145,7 +145,7 @@ class ItemORM(object):
 
             query += " {0} like '%{1}%'".format(item, query_params[item])
 
-        query += " limit 20"
+        query += " limit 10"
 
         return query
 
