ba27407ac4dc241214d5a64192eef5d829bbba79
Nick <nickpadge@gmail.com>
2015-10-31 22:55:45 -0400

    Updated admin utils to hopefully work.

--- Server/python/cgi-bin/adminUtils.py
+++ Server/python/cgi-bin/adminUtils.py
@@ -3,6 +3,6 @@ __author__ = 'nicholaspadgett'
 from DataSource import *
 
 def updateItem(id, creator):
-    sql = "update items set creator='"+creator+"' where id="+str(id)
+    sql = "update items set creator='{0}' where id={1}".format(creator, id)
     insert_query(sql)
     return True
